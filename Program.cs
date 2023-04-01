82472af08f201e9a6739eda971ae74365622e66e
gavvalaganeshbabu <ganeshgavvala@gmail.com>
2023-04-01 17:56:57 +0530

    Welcome master code Added

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineComparisionRemap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Line Comparision Computation :");
+        }
+    }
+}
